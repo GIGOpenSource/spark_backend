fdfef882d4a8b3f0c5901a6bd24e4f6f88dbc9c4
du-xing1 <535250071@qq.com>
2026-08-10 17:21:24 +0800

    11

--- frontend/mobile_matchup/api/maps.js
+++
@@ -1,9 +0,0 @@
-import { getRequest } from '@/utils/http.js'
-
-export function apiMapsGeocode(q) {
-	return getRequest('/maps/geocode/', { q }, { showLoading: false })
-}
-
-export function apiMapsRegeo(lat, lng) {
-	return getRequest('/maps/regeo/', { lat, lng }, { showLoading: false })
-}
